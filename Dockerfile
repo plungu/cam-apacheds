a95e6d5dd9ca0b3f6a0f127d058da8cb0eb82dbe
paul.lungu <paul.lungu@camunda.com>
2022-01-26 07:58:21 -0700

    update ldif

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM openjdk:11
 
 RUN apt-get update && apt-get install -y xinetd ldap-utils gettext-base
 
-ADD https://downloads.apache.org//directory/apacheds/dist/2.0.0.AM26/apacheds-2.0.0.AM26-amd64.deb /tmp/installer.deb
+ADD http://archive.apache.org/dist/directory/apacheds/dist/2.0.0.AM25/apacheds-2.0.0.AM25-amd64.deb /tmp/installer.deb
 
 RUN dpkg -i /tmp/installer.deb
 RUN mkdir /ldifs
 
 COPY ldifs/* /ldifs/
-COPY conf/wrapper.conf /opt/apacheds-2.0.0.AM26/conf
+COPY conf/wrapper.conf /opt/apacheds-2.0.0.AM25/conf
 
 EXPOSE 10389 10636 11001
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# /etc/init.d/apacheds-2.0.0.AM25-default start
-# /etc/init.d/xinetd start
-#
-# sleep 10
-#
-# export ADMIN_PASSWORD='secret'
-#
-# ldapmodify -c -a -f /ldifs/seed.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w 'secret'
-#
-# enable_replication
-# setup_replication
-#
-#
-# /etc/init.d/apacheds-2.0.0.AM25-default stop
-# /etc/init.d/apacheds-2.0.0.AM25-default console
+/etc/init.d/apacheds-2.0.0.AM25-default start
+/etc/init.d/xinetd start
+
+sleep 10
+
+export ADMIN_PASSWORD='secret'
+
+ldapmodify -c -a -f /ldifs/seed.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w 'secret'
+
+enable_replication
+setup_replication
+
+
+/etc/init.d/apacheds-2.0.0.AM25-default stop
+/etc/init.d/apacheds-2.0.0.AM25-default console
